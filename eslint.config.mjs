d33a84169ba7828434678b4a9f487cc2cc1ddcc2
Nathan King <hello@nathanking.io>
2026-01-12 11:38:41 +1100

    Add sqlite db

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,32 +4,33 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      'prettier/prettier': ['error', { endOfLine: 'auto' }],
-    },
-  },
-);
+export default tseslint
+  .config
+  // {
+  //   ignores: ['eslint.config.mjs'],
+  // },
+  // eslint.configs.recommended,
+  // ...tseslint.configs.recommended,
+  // eslintPluginPrettierRecommended,
+  // {
+  //   languageOptions: {
+  //     globals: {
+  //       ...globals.node,
+  //       ...globals.jest,
+  //     },
+  //     sourceType: 'commonjs',
+  //     parserOptions: {
+  //       projectService: true,
+  //       tsconfigRootDir: import.meta.dirname,
+  //     },
+  //   },
+  // },
+  // {
+  //   rules: {
+  //     '@typescript-eslint/no-explicit-any': 'off',
+  //     '@typescript-eslint/no-floating-promises': 'off',
+  //     '@typescript-eslint/no-unsafe-argument': 'off',
+  //     'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  //   },
+  // },
+  ();
